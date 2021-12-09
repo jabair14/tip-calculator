ba714c31999fd13d488cdf236cc702f605a98bfc
Jabair <jabairk@gmail.com>
2021-12-09 12:21:50 -0500

    localstorage done

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 
 document.getElementById("total").onchange = function(){
     let total = Number(document.getElementById('total').value)
-    console.log(total)
+    
 
     const fifteenPercent = document.getElementById('fifteen')
     const eighteenPercent = document.getElementById('eighteen')
     const twentyPercent = document.getElementById('twenty')
     const twentytwoPercent = document.getElementById('twentytwo')
 
-    // let amountDue = total 
+    // Basic Functionality 
 
     document.getElementById('fifteen').onclick = function(){
         let amountDue = total*1.15
@@ -19,19 +19,19 @@ document.getElementById("total").onchange = function(){
     document.getElementById('eighteen').onclick = function(){
         let amountDue = total*1.18
         let amountDueNumber = document.getElementById('amountDue')
-        amountDueNumber.innerHTML = amountDue
+        amountDueNumber.innerHTML = amountDue.toFixed(2)
     }
 
     document.getElementById('twenty').onclick = function(){
         let amountDue = total*1.2
         let amountDueNumber = document.getElementById('amountDue')
-        amountDueNumber.innerHTML = amountDue
+        amountDueNumber.innerHTML = amountDue.toFixed(2)
     }
 
     document.getElementById('twentytwo').onclick = function(){
         let amountDue = total*1.22
         let amountDueNumber = document.getElementById('amountDue')
-        amountDueNumber.innerHTML = amountDue
+        amountDueNumber.innerHTML = amountDue.toFixed(2)
     }
 
     document.getElementById('custom').onchange = function(){
@@ -41,10 +41,40 @@ document.getElementById("total").onchange = function(){
         amountDueNumber.innerHTML = amountDue.toFixed(2)
     }
 
-    // amountDue = document.createElement('h3')
+    // List items 
+    // myStorage = window.localStorage
+    // const savedBills = []
+    // document.addEventListener('ul')
+    // localStorage.setItem('billsList', 'somethingelse')
+    // console.log(savedBills)
+
+    let savedBills = []
+
+    
+
+    const saveButton = document.getElementById('add-to-list')
+
+    saveButton.addEventListener('click', function(){
+        
+        savedBills.push(amountDue.innerHTML)
+        localStorage.setItem('save', savedBills)
+
+        var node = document.createElement("li");
+        var textnode = document.createTextNode(amountDue.innerHTML);
+        node.appendChild(textnode);
+        document.getElementById("bills-list").appendChild(node);
+    })
+    
+    // document.getElementById('add-to-list').onClick = function(){
+    //     const str = JSON.stringify(savedBills)
+    //     localStorage.setItem('bills', str);
+    //     savedBills.push(amountDue)
+    //     console.log(savedBills)
+         
+    // }
+
+  
 }
 
 
 
-// let total = document.getElementById("total").value;
-// console.log(total)
